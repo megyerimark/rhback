f69592d4fe062befe4dff3f77ae6199ff827e488
Megyeri Márk Máté <megyko1234@gmail.com>
2026-05-15 22:01:27 +0200

    mail küldés sikeres

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage; 
+use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::createUrlUsing(function ($notifiable) {
+            $verifyUrl = URL::temporarySignedRoute(
+                'verification.verify',
+                now()->addMinutes(60),
+                [
+                    'id' => $notifiable->getKey(),
+                    'hash' => sha1($notifiable->getEmailForVerification()),
+                ]
+            );
+            return str_replace('http://localhost:8000/api', 'http://localhost:4200', $verifyUrl);
+        });
+
+
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('⛓️ Aktiváld a RaveHouse fiókodat! ⛓️')
+                ->greeting('Szia ' . $notifiable->name . '!')
+                ->line('Köszönjük, hogy regisztráltál a RaveHouse-ra. Már csak egyetlen lépés választ el attól, hogy beléphess a klubba.')
+                ->action('Fiók aktiválása', $url) // Ez lesz a kattintható nagy gomb
+                ->line('A biztonsági link 60 percig érvényes.')
+                ->line('Ha nem te indítottad a regisztrációt, nyugodtan hagyd figyelmen kívül ezt a levelet.')
+                ->salutation('Üdvözlettel, a RaveHouse csapata 🖤');
+        });
     }
 }
