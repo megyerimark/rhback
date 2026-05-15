e7bd470e59623ca0ef957e3bdec57834c10b7f3e
Megyeri Márk Máté <megyko1234@gmail.com>
2026-05-15 18:47:52 +0200

    Regisztáricó kipróbálása

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+       // Regisztráció
+
+       $validateData = $request->validate([
+
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => ['required','confirmed', Password::defaults()],
+       ]);  
+       $user = User::create([
+        'name'=>$validateData['name'],
+        'email'=>$validateData['email'],
+        'password'=>Hash::make($validateData['password']),
+       ]);
+
+       $user->assignRole('user');
+
+       event(new Registered($user));
+
+       return response()->json([
+        'message' => 'Sikeres regisztráció! Kérlek, ellenőrizd az e-mail fiókodat az aktiváláshoz.',
+        'user' => $user,
+       ], 201);
+    }
+    //email validáció
+       public function verify(Request $request, $id, $hash)
+       {
+        $user = User::findOrFail($id);
+        if (! hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
+            return response()->json(['message' => 'Érvénytelen vagy lejárt link.'], 400);
+        }
+        if ( $user->hasVerifiedEmail()) {
+            return response()->json(['message' => 'Ez a fiók már aktiválva van.'], 400);
+        }
+
+        $user->markEmailAsVerified();
+        return response()->json(['message' => 'Sikeres aktiválás! Most már bejelentkezhetsz.'], 200);
+        }
+
+        //Bejelentkezés
+        public function login(Request $request){
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            $user = User::where('email', $request->email)->first();
+
+
+            if(!user || ! Hash::check($request->password, $user->password))
+            {
+                return response()->json(['message' => 'Hibás email vagy jelszó.'], 401);
+            }
+
+            if (!$user->hasVerifiedEmail()) {
+                return response()->json(['message' => 'Kérlek, először aktiváld a fiókodat az emailben kapott linkre kattintva.'], 403);
+            }
+
+
+            $token = $user->createToken('ravehiuse_auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Sikeres bejelentkezés a RaveHouse-ba!',
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user, 
+                'role' => $user->getRoleNames(), 
+            ], 200);
+        }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+route::post('/register', [AuthController::class, 'register']);
+route::post('/login', [AuthController::class, 'login']);
+route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])->name('verification.verify');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+    
+});
